51e72c362b6a06687edfc71153feeca79caeffea
Jordan Jordanov <d_yugioh@abv.bg>
2019-02-17 13:15:35 +0200

    init

--- src/server/WeddingsPlanner.Data/Entities/Agency.cs
+++ src/server/WeddingsPlanner.Data/Entities/Agency.cs
@@ -1,10 +1,16 @@
-﻿namespace WeddingsPlanner.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WeddingsPlanner.Data.Entities
 {
     public class Agency
     {
+        [Key]
         public int Id { get; set; }
+
         public string Name { get; set; }
+
         public int EmployeesCount { get; set; }
+
         public string Town { get; set; }
     }
 }
--- src/server/WeddingsPlanner.Data/Entities/Venue.cs
+++ src/server/WeddingsPlanner.Data/Entities/Venue.cs
@@ -1,10 +1,19 @@
-﻿namespace WeddingsPlanner.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WeddingsPlanner.Data.Entities
 {
     public class Venue
     {
+        [Key]
         public int Id { get; set; }
+
         public string Name { get; set; }
+
+        /// <summary>
+        /// Number of people who can celebrate in that venue.
+        /// </summary>
         public int Capacity { get; set; }
+
         public string Town { get; set; }
     }
 }
