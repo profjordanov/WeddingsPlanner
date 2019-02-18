cae35b6eca4d63697a6616af5f08c961c3c3af5c
Jordan Jordanov <d_yugioh@abv.bg>
2019-02-19 00:19:37 +0200

    init

--- src/server/WeddingsPlanner.Data/Entities/Gift.cs
+++ src/server/WeddingsPlanner.Data/Entities/Gift.cs
@@ -1,8 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using WeddingsPlanner.Data.Enums;
 
 namespace WeddingsPlanner.Data.Entities
 {
+    [Table(nameof(Gift))]
     public class Gift : Present
     {
         /// <summary>
