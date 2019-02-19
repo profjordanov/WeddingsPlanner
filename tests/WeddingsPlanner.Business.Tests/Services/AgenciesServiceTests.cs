3dd872dbc4e4d1ba288b0ab63ba92cb8da04b7d4
Jordan Jordanov <d_yugioh@abv.bg>
2019-02-19 23:47:02 +0200

    Update AgenciesServiceTests.cs

--- tests/WeddingsPlanner.Business.Tests/Services/AgenciesServiceTests.cs
+++ tests/WeddingsPlanner.Business.Tests/Services/AgenciesServiceTests.cs
@@ -19,7 +19,7 @@ namespace WeddingsPlanner.Business.Tests.Services
     /// </summary>
     public class AgenciesServiceTests
     {
-        private readonly AgenciesService _agenciesService;
+        private AgenciesService _agenciesService;
         private readonly Mock<IMapper> _mapperMock;
 
         public AgenciesServiceTests()
@@ -62,6 +62,38 @@ namespace WeddingsPlanner.Business.Tests.Services
             result.MatchNone(error => error
                 .Messages
                 .ShouldAllBe(msg => msg == "Agency town cannot be empty!"));
+        }
+
+        [Theory]
+        [CustomAutoData]
+        public async Task AddAsync_Returns_None_If_Connection_To_Database_Is_Lost(Agency agency)
+        {
+            // Arrange
+            var wrongConnectionString =
+                "Server=x;Database=y;" +
+                "Integrated Security=True;" +
+                "Trusted_Connection=True;" +
+                "MultipleActiveResultSets=true;";
+
+            _agenciesService = new AgenciesService(
+                _mapperMock.Object,
+                DbContextProvider.GetSqlServerDbContext(wrongConnectionString));
+
+            // Act
+            var result = await _agenciesService.AddAsync(agency);
+
+            // Assert
+            result.HasValue.ShouldBe(false);
+            result.MatchNone(error => error
+                .Messages
+                .ShouldAllBe(msg => msg == "An unhandled exception has occurred while creating the agency : " +
+                                    "A network-related or instance-specific error occurred while " +
+                                    "establishing a connection to SQL Server. " +
+                                    "The server was not found or was not accessible. " +
+                                    "Verify that the instance name is correct and that SQL Server " +
+                                    "is configured to allow remote connections. " +
+                                    "(provider: Named Pipes Provider, error: 40 - " +
+                                    "Could not open a connection to SQL Server)!"));
         }
 
         [Theory]
