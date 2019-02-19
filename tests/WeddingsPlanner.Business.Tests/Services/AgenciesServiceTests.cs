5879a54539996518f3b01ec9f5639cf7804f375c
Jordan Jordanov <d_yugioh@abv.bg>
2019-02-19 23:08:18 +0200

    Update AgenciesServiceTests.cs

--- tests/WeddingsPlanner.Business.Tests/Services/AgenciesServiceTests.cs
+++ tests/WeddingsPlanner.Business.Tests/Services/AgenciesServiceTests.cs
@@ -3,14 +3,20 @@ using Microsoft.EntityFrameworkCore;
 using Moq;
 using Shouldly;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using WeddingsPlanner.Business.Services;
 using WeddingsPlanner.Business.Tests.Attributes;
 using WeddingsPlanner.Data.Entities;
 using Xunit;
+using Xunit.Sdk;
 
 namespace WeddingsPlanner.Business.Tests.Services
 {
+    /// <summary>
+    /// SQL Server Database Integration Tests
+    /// for <see cref="AgenciesService"/>. 
+    /// </summary>
     public class AgenciesServiceTests
     {
         private readonly AgenciesService _agenciesService;
@@ -26,7 +32,7 @@ namespace WeddingsPlanner.Business.Tests.Services
 
         [Theory]
         [CustomAutoData]
-        public async Task AddAsync_Works_Correctly(Agency agency)
+        public async Task AddAsync_Works_Correctly_For_Single_Record(Agency agency)
         {
             // Arrange
             agency.Id = 0;
@@ -42,6 +48,32 @@ namespace WeddingsPlanner.Business.Tests.Services
             agency.Name.ShouldBe(lastAgency.Name);
             agency.Town.ShouldBe(lastAgency.Town);
             agency.EmployeesCount.ShouldBe(lastAgency.EmployeesCount);
+        }
+
+        [Theory]
+        [CustomAutoData]
+        public async Task AddAsync_Works_Correctly_For_Multiple_Records(Agency[] agencies)
+        {
+            foreach (var agency in agencies)
+            {
+                // Arrange
+                agency.Id = 0;
+                agency.OrganizedWeddings = new HashSet<Wedding>();
+
+                // Act
+                var result = await _agenciesService.AddAsync(agency);
+
+                // Assert
+                result.HasValue.ShouldBe(true);
+            }
+
+            var lastAgencies = DbContextProvider
+                .GetSqlServerDbContext()
+                .Agencies
+                .OrderByDescending(agency => agency.Id)
+                .Take(agencies.Length);
+
+            lastAgencies.ShouldAllBe(agency => lastAgencies.Any(a => a.Name == agency.Name));
         }
     }
 }
