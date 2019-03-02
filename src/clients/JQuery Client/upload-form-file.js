012e041652ca134e0d738aada5be114ee845de50
Jordan Jordanov <d_yugioh@abv.bg>
2019-03-02 11:10:35 +0200

    init

--- src/clients/JQuery Client/upload-form-file.js
+++ src/clients/JQuery Client/upload-form-file.js
@@ -9,8 +9,16 @@ $("form#data").submit(function(e) {
     url: "http://localhost:5000/api/Onboarding/json-agencies",
     type: 'POST',
     data: formData,
-    success: function (data) {
-      alert(data)
+    xhrFields: {
+      responseType: 'blob'
+    },
+    success: function(data) {
+      let a = document.createElement('a');
+      let url = window.URL.createObjectURL(data);
+      a.href = url;
+      a.download = 'agencies-onboarding-' + Date.now() + '.csv';
+      a.click();
+      window.URL.revokeObjectURL(url);
     },
     cache: false,
     contentType: false,
